344bf2b62428804af025844e93deef93657c1e63
MAGNUS. W. HINGE <122803385+MacBeef@users.noreply.github.com>
2026-02-24 22:12:57 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import sidebar from './components/sidebar.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <sidebar/>
+  </main>
+</template>
+
---
+++ src/components/sidebar.vue
@@ -0,0 +1,33 @@
+<script setup>
+  /* import { } from 'lucide-vue-next'; */
+</script> 
+
+<template>
+  <main>
+    <section class="navSection">
+      <h1>Courses</h1>
+      <p>{{ moodle_data.course_name }}</p>
+    </section>
+  </main>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        moodle_data:[]
+      }
+    },
+
+    created(){
+      this.moodleAPI()
+    },
+
+    methods: {
+      async moodleAPI(){
+        let api = await fetch('https://lms.mironpingel.dk')
+        this.moodle_data = await api.json()
+      }
+    }
+  }
+</script>
